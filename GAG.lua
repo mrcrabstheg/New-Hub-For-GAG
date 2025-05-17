167dca7c8554423e257e805412ebf7971a4f64a5
mrcrabstheg <148804153+mrcrabstheg@users.noreply.github.com>
2025-05-17 21:35:22 +0200

    Update GAG.lua

--- GAG.lua
+++ GAG.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
+   Name = "GAG",
    Icon = nil, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Rayfield Interface Suite",
    LoadingSubtitle = "by Sirius",
